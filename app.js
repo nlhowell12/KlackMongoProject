194e62acf7f658c4001a0d1a35ea15ca15fa502e
Nick Howell <nlhowell12@gmail.com>
2018-05-15 13:47:35 -0400

    back to online paths

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(express.json())
 app.use(cors())
 
 // Mongo stuff
-// mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_URI}/${dbName}`, () => {
-// console.log("Successfully connected to database");
-// });
-mongoose.connect('mongodb://localhost/klack')
+mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_URI}/${dbName}`, () => {
+console.log("Successfully connected to database");
+});
+// mongoose.connect('mongodb://localhost/klack')
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
