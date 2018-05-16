a2975e7e66d40642aa86902b23bedb4ac706236f
Nick Howell <nlhowell12@gmail.com>
2018-05-16 16:12:19 -0400

    working as intended

--- public/main.js
+++ public/main.js
@@ -5,8 +5,8 @@ const ding = new Audio('typewriter_ding.m4a');
 const hamburger = document.getElementById('hamburger');
 
 // Connects to the server
-// const socket = io.connect('https://xforceklack.herokuapp.com/')
-const socket = io.connect("http://localhost:3000", {'sync disconnect on unload': true});
+const socket = io.connect('https://xforceklack.herokuapp.com/')
+// const socket = io.connect("http://localhost:3000", {'sync disconnect on unload': true});
 
 // text to emoji converter library
 const emoji = new EmojiConvertor();
